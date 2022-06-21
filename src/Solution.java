2cbc7ab708afa8316e15e09a690b89f2f9fc91cb
Lom911 <lomaev_82@mail.ru>
2022-06-21 07:19:23 +0300

    commit 1

--- src/Solution.java
+++
@@ -1,9 +0,0 @@
-public class Solution {
-    public static void main(String[] args) {
-        Window window = new Window();
-        window.printSize();
-        window.changeSize(4, 5);
-        window.printSize();
-    }
-}
-
