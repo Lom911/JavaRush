1ee555a46b341f02a8600a26b097039a3014447a
Lom911 <107207940+Lom911@users.noreply.github.com>
2022-06-22 17:03:52 +0300

    commit 1

---
+++ src/com/javarush/task/pro/task11/task1107/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.pro.task11.task1107;
+
+/*
+Двигатель — сердце автомобиля
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+    }
+}
+
+
+
+
