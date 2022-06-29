13998930cd1ae3e1e65ece6b16c92c106ff30cb9
Lom911 <107207940+Lom911@users.noreply.github.com>
2022-06-29 17:45:55 +0300

    commit 1

--- src/Solution.java
+++ src/Solution.java
@@ -1,26 +1,23 @@
-import java.security.MessageDigest;
-import java.util.ArrayList;
-import java.util.Scanner;
-
+import java.math.BigInteger;
 public class Solution {
 
-    public static boolean doubleExpression(double a, double b, double c) {
-        double z = a + b;
-//        if (c == d)
-//        return true;
-//        else
-//            return false;
-        boolean b1 = (Math.abs(c) == Math.abs(z));
-        return b1;
-    }
 
+    public static BigInteger factorial(int value) {
+        BigInteger intRezul = new BigInteger("1");
+        for (int i = 1; i < value + 1; i++) {
+            BigInteger cikl = new BigInteger("" + (i));
+            BigInteger intRezul2 = intRezul.multiply(cikl);
+            return intRezul2;
+         }
+
+    }
 
     public static void main(String[] args) {
-        double a = 8.50;
-        double b = 8.55;
-        double c = 17.0;
-        System.out.println(doubleExpression(a, b, c));
-    }
+        int value = 3;
+        System.out.println(factorial(value));
     }
 
 
+
+
+} // не удалять
